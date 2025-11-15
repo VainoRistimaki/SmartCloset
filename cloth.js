8d543d86f3fdf30057e447d226f371ba5e3c167e
csbin0830 <csbin0830@kaist.ac.kr>
2025-11-15 10:34:46 +0900

    Add files via upload

---
+++ cloth.js
@@ -0,0 +1,102 @@
+[
+  {
+    "id": 1,
+    "name": "Black Coat",
+    "type": "outer",
+    "thickness": 4,
+    "color": "black",
+    "formality": "formal"
+  },
+  {
+    "id": 2,
+    "name": "White Puffer Jacket",
+    "type": "outer",
+    "thickness": 5,
+    "color": "white",
+    "formality": "casual"
+  },
+  {
+    "id": 3,
+    "name": "Sky Blue Knit",
+    "type": "top",
+    "thickness": 3,
+    "color": "skyblue",
+    "formality": [ "casual", "formal"]
+  },
+  {
+    "id": 4,
+    "name": "Pink Knit",
+    "type": "top",
+    "thickness": 3,
+    "color": "pink",
+    "formality": [ "casual", "formal"]
+  },
+  {
+    "id": 5,
+    "name": "White Blouse",
+    "type": "top",
+    "thickness": 1,
+    "color": "white",
+    "formality": "formal"
+  },
+  {
+    "id": 6,
+    "name": "Beige Sweatshirt",
+    "type": "top",
+    "thickness": 2,
+    "color": "beige",
+    "formality": "casual"
+  },
+  {
+    "id": 7,
+    "name": "White Short-Sleeve Tee",
+    "type": "top",
+    "thickness": 1,
+    "color": "white",
+    "formality": "casual"
+  },
+  {
+    "id": 8,
+    "name": "Grey Short-Sleeve Tee",
+    "type": "top",
+    "thickness": 1,
+    "color": "grey",
+    "formality": "casual"
+  },
+  {
+    "id": 9,
+    "name": "Black Bootcut Pants",
+    "type": "bottom",
+    "thickness": 2,
+    "color": "black",
+    "formality": [ "casual", "formal"
+    ]
+  },
+  {
+    "id": 10,
+    "name": "Gray Sweatpants",
+    "type": "bottom",
+    "thickness": 2,
+    "color": "gray",
+    "formality": "casual"
+  },
+  {
+    "id": 11,
+    "name": "Black Pleated Skirt",
+    "type": "bottom",
+    "thickness": 2,
+    "color": "black",
+    "formality": [
+      "casual",
+      "formal"
+    ]
+  },
+  {
+    "id": 12,
+    "name": "Blue Jeans",
+    "type": "bottom",
+    "thickness": 2,
+    "color": "blue",
+    "formality": "casual"
+  }
+]
