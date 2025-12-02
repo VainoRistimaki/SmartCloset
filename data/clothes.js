8277bd45030bdabba7507d7054fd624a74646f70
sohyeonYu <anna5031@kaist.ac.kr>
2025-12-02 17:55:04 +0900

    10 clothes list

---
+++ data/clothes.js
@@ -0,0 +1,94 @@
+[
+  {
+    "id": 0,
+    "name": "Black Coat",
+    "type": "outer",
+    "thickness": 4,
+    "color": "black",
+    "formality": "formal",
+    "style": ["chic", "minimal"]
+  },
+  {
+    "id": 1,
+    "name": "White Puffer Jacket",
+    "type": "outer",
+    "thickness": 5,
+    "color": "white",
+    "formality": "casual",
+    "style": ["casual", "minimal"]
+  },
+
+  {
+    "id": 2,
+    "name": "Sky Blue Knit",
+    "type": "top",
+    "thickness": 3,
+    "color": "skyblue",
+    "formality": ["casual", "formal"],
+    "style": ["cute", "minimal"]
+  },
+  {
+    "id": 3,
+    "name": "White Blouse",
+    "type": "top",
+    "thickness": 1,
+    "color": "white",
+    "formality": "formal",
+    "style": ["feminine", "formal"]
+  },
+  {
+    "id": 4,
+    "name": "Grey Sweatshirt",
+    "type": "top",
+    "thickness": 2,
+    "color": "beige",
+    "formality": "casual",
+    "style": ["casual", "minimal"]
+  },
+  {
+    "id": 5,
+    "name": "Black Long-Sleeve Tee",
+    "type": "top",
+    "thickness": 2,
+    "color": "black",
+    "formality": "casual",
+    "style": ["minimal", "sporty"]
+  },
+  {
+    "id": 6,
+    "name": "Red Cardigan",
+    "type": "top",
+    "thickness": 2,
+    "color": "red",
+    "formality": "casual",
+    "style": ["casual", "feminine"]
+  },
+
+  {
+    "id": 7,
+    "name": "Black Bootcut Pants",
+    "type": "bottom",
+    "thickness": 2,
+    "color": "black",
+    "formality": ["casual", "formal"],
+    "style": ["minimal", "feminine"]
+  },
+  {
+    "id": 8,
+    "name": "Black Pleated Skirt",
+    "type": "bottom",
+    "thickness": 1,
+    "color": "black",
+    "formality": ["casual", "formal"],
+    "style": ["feminine"]
+  },
+  {
+    "id": 9,
+    "name": "Black Leggings",
+    "type": "bottom",
+    "thickness": 2,
+    "color": "black",
+    "formality": "casual",
+    "style": ["sporty", "minimal"]
+  }
+]
